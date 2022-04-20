cf30ce2ad8407412bf88879b6748c84979b2bd26
aref abolhasani <arefabolhasani21@gmail.com>
2022-04-20 12:27:44 +0430

    add navabar and responsive that

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import './Navbar.css'
+
+export default function Navbar() {
+  return (
+    <div className='navbar-class'>
+        <h2>Virlog Page</h2>
+        <button>new qoute</button>
+    </div>
+  )
+}
